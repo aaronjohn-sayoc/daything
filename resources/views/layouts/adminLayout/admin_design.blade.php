174ab23f737f45659c3c40dfd6ca63a1fa50d182
Aaron John B. Sayoc <aaronjohn.sayoc@gmail.com>
2019-02-07 23:04:19 +0800

    ajax insecure

--- resources/views/layouts/adminLayout/admin_design.blade.php
+++ resources/views/layouts/adminLayout/admin_design.blade.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <!-- Tell the browser to be responsive to screen width -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0 shrink-to-fit=no">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"> 
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="description" content="">
     <meta name="author" content="">
