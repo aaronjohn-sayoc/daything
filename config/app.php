48df521dfa956cb99dc599ff151fa1b7114f8198
aaronjohn-sayoc <aaronjohn.sayoc@gmail.com>
2019-02-07 15:17:16 +0800

    added photos folder

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://http://mysterious-taiga-19135.herokuapp.com'),
+    'url' => env('APP_URL', 'http://mysterious-taiga-19135.herokuapp.com'),
 
     'asset_url' => env('ASSET_URL', null),
 
